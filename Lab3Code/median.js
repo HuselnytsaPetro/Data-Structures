59170c91cdb8eb44fd22da1b334d9b023c888519
HuselnytsaPetro <huselnytsia.petro@student.uzhnu.edu.ua>
2025-03-15 18:28:00 +0200

    gg

---
+++ Lab3Code/median.js
@@ -0,0 +1,22 @@
+function CalculateMedian(array) {
+    if (array.length === 0) return null;
+
+    for (let count = 0; count < array.length - 1; count++) {
+        let replaced = false;
+
+        for (let index = 0; index < array.length - 1 - count; index++) {
+            if (array[index] > array[index + 1]) {
+                let tmp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = tmp;
+                replaced = true;
+            }
+        }
+
+        if (!replaced)
+            break;
+    }
+
+    return array.length % 2 !== 0 ? array[Math.floor(array.length / 2)] : (array[array.length / 2] + array[(array.length / 2) - 1]) / 2;
+};
+console.log(CalculateMedian([1, 2, 3, 4, 5, 6, 7]));
